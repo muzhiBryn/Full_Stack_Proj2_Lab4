d782feb57ae43e08d28781d215d7ee12c78496fc
luckyjialing <jialing.wu.gr@dartmouth.edu>
2020-05-14 13:31:37 -0400

    connect with lab 5

--- src/cs52-api.js
+++ src/cs52-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const ROOT_URL = 'https://platform.cs52.me/api';
-// const ROOT_URL = 'https://lab5-1.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://lab5-1.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 const API_KEY = '?key=JIALING';
 
 export const fetchPostsApi = () => {
